7af0e44645a8c4f484d3a3c5c4d394c71e6f9ba7
Atishay Jain <atishay2007@gmail.com>
2026-07-28 19:25:41 +0530

    update homepage styling

--- lib/posts.ts
+++ lib/posts.ts
@@ -1,59 +1,65 @@
 import { supabase } from "@/lib/supabase";
+import { unstable_noStore as noStore } from "next/cache";
+
 
 export type Post = {
-  id: number;
-  fileId: string;
-  title: string;
-  slug: string | null;
-  date: string;
-  content: string;
-  author?: string | null;
+    id: number;
+    fileId: string;
+    title: string;
+    slug: string | null;
+    date: string;
+    content: string;
+    author?: string | null;
 };
 
 
 export async function getAllPosts(): Promise<Post[]> {
-  const { data, error } = await supabase
-    .from("posts")
-    .select("*")
-    .order("date", {
-      ascending: false,
-    });
+    noStore();
 
-  if (error) {
-    console.error(error);
-    return [];
-  }
+    const { data, error } = await supabase
+        .from("posts")
+        .select("*")
+        .order("date", {
+            ascending: false,
+        });
 
-  return data.map((post) => ({
-    ...post,
-    fileId: String(post.id),
-  }));
+    if (error) {
+        console.error(error);
+        return [];
+    }
+
+    return data.map((post) => ({
+        ...post,
+        fileId: String(post.id),
+    }));
 }
 
 
 export async function getPostById(
-  id: string
+    id: string
 ): Promise<Post | null> {
 
-  const { data, error } = await supabase
-    .from("posts")
-    .select("*")
-    .eq("id", Number(id))
-    .single();
+    const { data, error } = await supabase
+        .from("posts")
+        .select("*")
+        .eq("id", Number(id))
+        .single();
 
-  if (error || !data) {
-    return null;
-  }
+    if (error || !data) {
+        return null;
+    }
 
-  return {
-    ...data,
-    fileId: String(data.id),
-  };
+    return {
+        ...data,
+        fileId: String(data.id),
+    };
 }
 
 
 export async function getLatestPosts(limit = 10) {
-  const posts = await getAllPosts();
+    noStore();
 
-  return posts.slice(0, limit);
+    const posts = await getAllPosts();
+
+    return posts.slice(0, limit);
 }
