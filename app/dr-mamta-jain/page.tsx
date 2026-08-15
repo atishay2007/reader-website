4f0b4c3e2a646aa73a450eb561d28839618f1342
Atishay Jain <atishay2007@gmail.com>
2026-08-15 15:41:35 +0530

    Add standalone Dr. Mamta Jain page

---
+++ app/dr-mamta-jain/page.tsx
@@ -0,0 +1,14 @@
+export default function DrMamtaJainPage() {
+    return (
+        <iframe
+            src="/mamta/index.html"
+            title="Dr. Mamta Jain"
+            style={{
+                width: "100vw",
+                height: "100vh",
+                border: "none",
+                display: "block",
+            }}
+        />
+    );
+}
